8d9f4c4c4f30b4513c3616903f4d41c28eb6c240
Pau Olivé <rcolivepau@gmail.com>
2019-04-05 13:00:37 +0200

    working onn it

--- OpenGLExercise/main.cpp
+++ OpenGLExercise/main.cpp
@@ -10,6 +10,12 @@ int main(int argc, char *argv[])
     format.setMinorVersion(3);
     format.setMajorVersion(3);
     format.setProfile(QSurfaceFormat::CoreProfile);
+    format.setDepthBufferSize(24);
+    format.setRedBufferSize(8);
+    format.setGreenBufferSize(8);
+    format.setBlueBufferSize(8);
+    format.setAlphaBufferSize(8);
+    format.setSwapBehavior(QSurfaceFormat::SwapBehavior::DoubleBuffer);
     format.setOption(QSurfaceFormat::DebugContext);
     QSurfaceFormat::setDefaultFormat(format);
 
